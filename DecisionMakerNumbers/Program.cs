414b9d611a54c763c5788f14701266e672f07377
Grace <graceseymore1@gmail.com>
2021-01-21 16:22:30 -0500

    first commit

---
+++ DecisionMakerNumbers/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Linq;
+namespace DecisionMakerNumbers
+
+{
+         class MainClass
+        {
+            public static void Main(string[] args)
+            {
+
+
+
+                Console.WriteLine("Hello there! What's your name?");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Thanks " + (name) + ". Please enter a number between 1 and 100.");
+
+                string input = Console.ReadLine();
+                int num = int.Parse(input);
+
+                //Console.WriteLine(num);
+
+
+
+
+                {
+
+
+                    if (num >= 1 && num <= 100)
+                    {
+                        //Console.WriteLine("Number accepted.");
+
+
+                        int EvenOrOdd = (num % 2);
+
+                        if (EvenOrOdd == 1)
+                        {
+                            Console.WriteLine("Odd " + num);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Even");
+
+                            {
+                                if (num >= 2 && num <= 24)
+                                {
+                                    Console.WriteLine("and less than 25");
+                                }
+                                else if (num >= 61)
+                                {
+                                    Console.WriteLine(num);
+                                }
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again.");
+
+                    }
+
+
+
+
+
+                }
+
+            }
+        }
+    }
